43a92a42c96f043f8225b16f925d7ef20bb7f478
kaitojim <kaitojim@gmail.com>
2026-04-19 14:07:20 +0700

    Delete stuff

--- cppBackEnd/Tools/build/CMakeFiles/Teacher_tools.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libTeacher_tools.a"
-  "libTeacher_tools.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Teacher_tools.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cppBackEnd/Tools/build/Element/CMakeFiles/ElementClass.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for ElementClass.
-# This may be replaced when dependencies are built.

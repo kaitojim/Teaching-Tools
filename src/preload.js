bf1a33add7a4f1f37e61125401ab95c874496a6b
kaitojim <kaitojim@gmail.com>
2026-04-19 13:29:55 +0700

    Update JS logic

--- src/preload.js
+++ src/preload.js
@@ -1,1 +1,6 @@
 module.exports = {};
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('cppAPI', {
+  transform: (text, selections, val) => ipcRenderer.invoke('transform-element', text, selections, val)
+});

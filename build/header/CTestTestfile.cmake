4206fbe4e7d036bba84923f9ac1bf97f611863ba
kaitojim <kaitojim@gmail.com>
2026-04-25 21:58:54 +0700

    Update logic

---
+++ build/header/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ADMIN/Documents/Teacher_tools/V4/teaching/cppBackEnd/header
+# Build directory: C:/Users/ADMIN/Documents/Teacher_tools/V4/teaching/build/header
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ADMIN/Documents/Teacher_tools/V4/teaching/cppBackEnd/src
+# Build directory: C:/Users/ADMIN/Documents/Teacher_tools/V4/teaching/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
